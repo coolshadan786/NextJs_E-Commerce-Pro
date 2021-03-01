8db56546041e1dd65af101998d5154f79dbf4ea1
coolshadan786 <coolshadan786@gmail.comgit>
2021-03-01 15:22:52 +0530

    first commit

---
+++ components/Layout/components/FooterCopyright/FooterCopyright.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function FooterCopyright() {
+  return (
+    <div className="footer-copyright">
+      <p className="footer-company">
+        All Rights Reserved. &copy; 2021 <a href="#">ThewayShop</a> Design By :
+        <a href="https://www.linkedin.com/in/md-umair-abdullah-0a8a94178">
+          Design & Created by Um@ir
+        </a>
+      </p>
+    </div>
+  );
+}
